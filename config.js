4dc88e3b9a6969f4dce242ce7484135cbafa43f2
ravecavemanager-a11y <ravecavemanager@gmail.com>
2026-08-20 14:14:35 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+// Google Apps Script (GAS) のデプロイURLを設定します
+const CONFIG = {
+  // 後ほどGASを作成した後に取得できるWeb App URLをここに貼り付けます
+  GAS_API_URL: "https://script.google.com/macros/s/AKfycbzubvZp6J_l7gLE5fm4nViamXSLna7RmbymvQIRb0_IEJ5g5gKE1gM2VNMQUTbTMgM/exec",
+  // 自動更新のチェック間隔（ミリ秒）： 30,000ms = 30秒
+  AUTO_REFRESH_INTERVAL: 30000
+};
